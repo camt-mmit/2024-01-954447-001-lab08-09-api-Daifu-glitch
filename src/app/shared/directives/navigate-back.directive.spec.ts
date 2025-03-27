1f879a73c694606073a20ecaa7f8100a6b0c2c27
Daifu-glitch <w.aonpiam@gmail.com>
2025-03-27 15:40:02 +0700

    Finsihed

--- src/app/shared/directives/navigate-back.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NavigateBackDirective } from './navigate-back.directive';
-
-describe('NavigateBackDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NavigateBackDirective();
-    expect(directive).toBeTruthy();
-  });
-});
